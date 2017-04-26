2e90ea72791bf2d80bb5951e74f4e816cf6a4443
Brent Ian Luis <brentianluis@gmail.com>
2017-04-26 23:33:48 +0800

    Added file output function

--- n-chancellors-solver.c
+++ n-chancellors-solver.c
@@ -1,92 +1,71 @@
 /**
  * Authors:
- * Name
- * Name
- * Name
+ * Jaro, Jeriel
+ * Luis, Brent Ian
+ * Rivera, Marianne Louise
  * CMSC 142 C-3L
  *
  * Program Description:
- * ...
+ * A program that solves the N-Chancellor Problem and its modification by providing all possible solutions and their count using iterative backtracking algoritm
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-//iterative backtracking in getting the number of solutions
+//iterative backtracking function in getting the number of solutions
 int backtracking(int *board, int dimension){
-	int start, move;
-	int nopts[dimension+2];		//array top of stacks
-	int option[dimension+2][dimension+2];	//array stacks of options
-	int i, j, candidate, flag, num_of_solutions;
-
-	//init option
-	for(i=0; i<dimension+2; i++){
-		for(j=0; j<dimension+2; j++){
-			option[i][j] = 0;
-		}//for
-	}//for
-
-	//init nopts to 0
-	for(i=0; i<dimension+2; i++){
-		nopts[i] = 0;
-	}//for
-
-	move = start = 0; 
-	nopts[start] = 1;
-
-/*
-	while(nopts[start] >0){
-		if(nopts[move]>0){
-
-		} else{		//backtrack
-
-		}//else
-	}//while
-*/
+	int num_of_solutions;
 
 	return num_of_solutions;
 }//backtracking
 
 int main(){
-	int num_of_puzzles, dimension, i, j, num_of_solutions;
+	int num_of_puzzles, num_of_solutions;
+	int dimension, puzzle;
+	int i, j;
 
-	FILE *fp = fopen("input.in", "r");
+	FILE *fp_input, *fp_output;
 
-	fscanf(fp, "%d", &num_of_puzzles);
+	fp_input = fopen("input.txt", "r");
 
-	if(fp == NULL){		//detects no file or error file reading
+	fscanf(fp_input, "%d", &num_of_puzzles);
+
+	if(fp_input == NULL){		//detects no file or error file reading
 		printf("Error file reading!\n");
 	} else{
-		while(!feof(fp)){
-			fscanf(fp, "%d", &dimension);
+		for(puzzle=0; puzzle<num_of_puzzles; puzzle++){
+			fscanf(fp_input, "%d", &dimension);
 
-			//initializes and allocates board base from the dimension
+			//initializes and allocates the board base from the dimension
 		    int *board[dimension];
 		    for(i=0; i<dimension; i++){
 		        board[i] = (int *)malloc(dimension * sizeof(int));
 		    }//for
 
-		    //scans puzzle and stores them into board 2d array
+		    //scans matrix input and stores them into the board 2d array
 		    for(i=0; i<dimension; i++){
 		     	for (j=0; j<dimension; j++){
-		        	fscanf(fp, "%d ", &board[i][j]);
+		        	fscanf(fp_input, "%d ", &board[i][j]);
 		     	}//for
 		    }//for
 
-		    //prints initial positions of the chancellors
+		    /* prints the 2d array of the initial positions of the chancellors
+		     * for checking lang kung tama yung na-read sa input
 		    for(i=0; i<dimension; i++){
 		     	for (j=0; j<dimension; j++){
-		        	printf("%d ", board[i][j]);
-		     	} printf("\n");
+		        	fprintf(fp_output,"%d ", board[i][j]);
+		     	} fprintf(fp_output, "\n");
 		    }//for
+		    */
 
-			//prints number of solutions of the corresponding board
-		    num_of_solutions = backtracking(*board, dimension);
-		    printf("%d solutions\n\n", num_of_solutions);
+		    //outputs the number of solutions to text file
+		    fp_output = fopen("output.txt", "a");
+		    fprintf(fp_output, "Puzzle %d: %d solutions\n", puzzle+1, num_of_solutions);
+		    fclose(fp_output);
 		}//while
 	}//else
 
-	fclose(fp);
+	fclose(fp_input);
 
 	return 0;
 }//main
